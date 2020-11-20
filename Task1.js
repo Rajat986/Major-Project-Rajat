308ddf8c3bcf9db29010fbac96095a64ba22a051
Rajat Sharma <36686400+Rajat986@users.noreply.github.com>
2020-11-20 17:25:05 +0530

    Update Task1.js

--- Task1.js
+++ Task1.js
@@ -5,7 +5,7 @@ var util = require('util');
 var log_file = fs.createWriteStream(__dirname + '/data.txt', {flags : 'w'});
 var log_stdout = process.stdout;
 
-printt = function write_to_file(d) { 
+printt = function(d) { 
   log_file.write(util.format(d) + '\n');
   log_stdout.write(util.format(d) + '\n');
 };
@@ -20,9 +20,11 @@ function comments()
 	printt(git.command('log --pretty="%s:%b"'));
 }
 comments()
-var server = http.createServer(function file_contents(req, res) {
+var server = http.createServer(function(req, res) {
     fs.readFile(__dirname + '/data.txt', function (err, data) {
         res.end(data);
     });
 });
 server.listen(8000);
+
+module.exports = { printt, comments};
