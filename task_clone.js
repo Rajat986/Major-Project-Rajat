d599ba47b0f64002c564fb6d36b328a4f75a4232
Rajat Sharma <36686400+Rajat986@users.noreply.github.com>
2021-01-27 12:12:59 +0530

    Update task_clone.js

--- task_clone.js
+++ task_clone.js
@@ -1,9 +1,15 @@
 var Git = require('nodegit');
+var path = require('path');
 url = 'https://github.com/Rajat986/Test-Repo.git';
 
 function clone_git_repo(url)
 {
-    Git.Clone(url, 'tmp')
+    Git.Clone(url, 'tmp').then(function(repo){
+            console.log("Cloning successful!")
+            console.log("Cloned " + path.basename(url) + " to " + repo.workdir());
+        }).catch(function (err) {
+            console.log(err);
+    })
 }
-
+clone_git_repo(url);
 module.exports = {clone_git_repo};
