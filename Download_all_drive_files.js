7ef355932f30958262e7d97ae645d904f03bd9af
Rajat Sharma <36686400+Rajat986@users.noreply.github.com>
2021-03-09 13:20:49 +0530

    Update Download_all_drive_files.js

--- Download_all_drive_files.js
+++ Download_all_drive_files.js
@@ -14,8 +14,8 @@ const TOKEN_PATH = 'token.json';
 // Load client secrets from a local file.
 fs.readFile('credentials.json', (err, content) => {
   if (err) return console.log('Error loading client secret file:', err);
-  // Authorize a client with credentials, then call the Google Docs API.
-  authorize(JSON.parse(content), activity_doc_log);
+  // Authorize a client with credentials, then call the Google Drive API.
+  authorize(JSON.parse(content), listFiles);
 });
 
 /**
@@ -31,7 +31,7 @@ function authorize(credentials, callback) {
 
   // Check if we have previously stored a token.
   fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getNewToken(oAuth2Client, callback);
+    if (err) return getAccessToken(oAuth2Client, callback);
     oAuth2Client.setCredentials(JSON.parse(token));
     callback(oAuth2Client);
   });
@@ -43,7 +43,7 @@ function authorize(credentials, callback) {
  * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
  * @param {getEventsCallback} callback The callback for the authorized client.
  */
-function getNewToken(oAuth2Client, callback) {
+function getAccessToken(oAuth2Client, callback) {
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: 'offline',
     scope: SCOPES,
@@ -60,7 +60,7 @@ function getNewToken(oAuth2Client, callback) {
       oAuth2Client.setCredentials(token);
       // Store the token to disk for later program executions
       fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-        if (err) console.error(err);
+        if (err) return console.error(err);
         console.log('Token stored to', TOKEN_PATH);
       });
       callback(oAuth2Client);
@@ -69,69 +69,49 @@ function getNewToken(oAuth2Client, callback) {
 }
 
 /**
- * Prints the title of a sample doc:
- * https://docs.google.com/document/d/195j9eDD3ccgjQRttHhJPymLJUCOUjs-jmwTrekvdjFE/edit
- * @param {google.auth.OAuth2} auth The authenticated Google OAuth 2.0 client.
+ * Lists the names and IDs of up to 10 files.
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
-async function activity_doc_log(auth, callback) {
-  //Lists 10 activities of a student given in 'name'
-  stu_email="'hithesh9591@gmail.com' in writers";
-  email="hithesh9591@gmail.com";
-  nameS="Hithesh Kumar N";
-  stu_identify="name contains 'Ujjwal'";
-  var log_file = fs.createWriteStream(__dirname + '/drive_files.txt', {flags : 'w'});
+function listFiles(auth) {
+    var log_file = fs.createWriteStream(__dirname + '/drive_files.txt', {flags : 'a'});
 
-  const drive = google.drive({version: 'v3', auth});
-  const driveactivity=google.driveactivity({version: 'v2', auth});
-
-  const fileList=[];
-  let NextPageToken="";
-  do{
-    const params = {
-      //q:stu_email,
-      q:"not 'rsrajat123456789@gmail.com' in writers",
-      orderBy: "name",
-      pageToken: NextPageToken || "",
-      pageSize: 1000,
-      fields: "nextPageToken, files/owners, files/id, files/name"
-    };
-    const res=await drive.files.list(params);
-    Array.prototype.push.apply(fileList, res.data.files);
-    NextPageToken=res.data.nextPageToken;
-  } while (NextPageToken);
-  console.log("Total number of files in my drive are: "+fileList.length);
-  fileList.map((file) => {
-    //console.log(`${file.name} (${file.id})`)
-    var owners=file.owners;
-    owners.map((owner)=>{
-      //if(`${owner.emailAddress}` == email)
-      {
-        driveactivity.activity.query({
-          requestBody:{
-            itemName:`items/${file.id}`
-          }
-        },(err, res)=>{
-          if(err) console.log("Activity API returned error: "+err);
-          const activities=res.data.activities;
-          s=`${owner.emailAddress} [${file.name}] (${file.id}) `;
-          if(activities.length && `${owner.emailAddress}` !== 'rsrajat123456789@gmail.com') {
-            activities.map((activity) => {
-              //console.log(`${activity.timestamp}`);
-              const targetS=activity.targets;
-              targetS.map((targets)=>{
-                const drive_item=targets.driveItem;
-                s=s+`${activity.timestamp} ${drive_item.owner.user.knownUser.personName} `;
-              })
-            })
-          }
-          console.log(s);
-          console.log();
-          log_file.write(util.format(s + '\n'));
-        })
-        //console.log(`${owner.emailAddress} [${file.name}] (${file.id})`);
-        //log_file.write(util.format(`${owner.emailAddress} [${file.name}] (${file.id})`+ '\n'));
-      }
-    })
-  })
-
+    const drive = google.drive({version: 'v3', auth});
+    const driveactivity=google.driveactivity({version: 'v2', auth});
+    drive.files.list({
+        orderBy: "name",
+        pageSize: 1000,
+        pageToken: "~!!~AI9FV7RoPbrOvGTSPtxspPKefBId8KM5AHBdHs4ck5-QZry-1UuM2fA4U7g5EXbONjHhPzbtA1CeqAuq6m6V4fyhVqeW1fI_RZPEmAtvtO081y4d_7xC_Mb6QDAQwWU4tRFeV6mRgOSeQ9a6NVLnuRejjEn9nu5pbpo4v7iW6_H6fCkhrEtV772XvCNDvh9iKF2PLhhPREhw-Y06vubJO8xQQXqRPKZzKD7RN5Utq91m2iROHMCB9SUz7ttWuUYxUC9CZVn8LA_NuvzIfGCmT2eEnUpZX1-8ZawRIWBfKhUjAzP2_4F4FKfahbqc5MFV5SNIOFz0Db0ep3KZm53esmoWNQDBILDUlyfkgqvkpODiO8o8QTn7NfbufQfQ8P9HU8v7UnZDNb3SdZKN00UtJvqbMr2JeieBijNlEDxzSirf9uRni3O5HBLn0c7iQpYccTAdxlWzD-lKCWdGY09KWgmZ-YM2N0f8UfkuQW54ThKKjDZ69Jft1SM=",
+        q:"not 'rsrajat123456789@gmail.com' in writers",
+        fields:"nextPageToken, files/owners, files/id, files/name",
+    },(err, res1)=> {
+        if (err) return console.log('The API returned an error: ' + err);
+        const files = res1.data.files;
+        if(files.length) {
+            files.map((file)=> {
+                var owners=file.owners;
+                owners.map((owner)=>{
+                    driveactivity.activity.query({
+                        requestBody:{
+                            itemName:`items/${file.id}`
+                        }
+                    },(err,res2) => {
+                        if(err) console.log("Activity API returned error: "+err);
+                        const activities=res2.data.activities;
+                        s=`${owner.emailAddress}$%[${file.name}]#@(${file.id})!*`;
+                        if(activities.length) {
+                            activities.map((activity)=> {
+                                const target_S=activity.targets;
+                                target_S.map((targets)=>{
+                                    const drive_item=targets.driveItem;
+                                    s=s+`${activity.timestamp}-${drive_item.owner.user.knownUser.personName}*&*`;
+                                })
+                            })
+                        }
+                        console.log(s);
+                        log_file.write(s + '\n');
+                    })
+                })
+            });
+        }
+    });
 }
