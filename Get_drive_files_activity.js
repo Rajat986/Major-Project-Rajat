111e3988b5f15bc935b820a3b6f60673cc61b410
Rajat Sharma <36686400+Rajat986@users.noreply.github.com>
2021-04-10 09:50:43 +0530

    Update Get_drive_files_activity.js

--- Get_drive_files_activity.js
+++ Get_drive_files_activity.js
@@ -6,7 +6,6 @@ const { file } = require('googleapis/build/src/apis/file');
 const { promisify } = require('util')
 const sleep = require('sleep-async')();
 const sleep1=require('sleep-promise');
-
 //var drive_file=fs.createWriteStream(__dirname+'/drive_files.txt',{flags:'w'});
 var drive_activity_file=fs.createWriteStream(__dirname+'/drive_activity_files.txt',{flags:'w'});
 // If modifying these scopes, delete token.json.
@@ -15,14 +14,12 @@ const SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly https:/
 // created automatically when the authorization flow completes for the first
 // time.
 const TOKEN_PATH = 'token.json';
-
 // Load client secrets from a local file.
 fs.readFile('credentials.json', (err, content) => {
   if (err) return console.log('Error loading client secret file:', err);
   // Authorize a client with credentials, then call the Google Drive API.
   authorize(JSON.parse(content), listFiles);
 });
-
 /**
  * Create an OAuth2 client with the given credentials, and then execute the
  * given callback function.
@@ -33,7 +30,6 @@ function authorize(credentials, callback) {
   const {client_secret, client_id, redirect_uris} = credentials.installed;
   const oAuth2Client = new google.auth.OAuth2(
       client_id, client_secret, redirect_uris[0]);
-
   // Check if we have previously stored a token.
   fs.readFile(TOKEN_PATH, (err, token) => {
     if (err) return getAccessToken(oAuth2Client, callback);
@@ -41,7 +37,6 @@ function authorize(credentials, callback) {
     callback(oAuth2Client);
   });
 }
-
 /**
  * Get and store new token after prompting for user authorization, and then
  * execute the given callback with the authorized OAuth2 client.
@@ -72,7 +67,6 @@ function getAccessToken(oAuth2Client, callback) {
     });
   });
 }
-
 /**
  * Lists the names and IDs of up to 10 files.
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
@@ -80,12 +74,10 @@ function getAccessToken(oAuth2Client, callback) {
 async function listFiles(auth) {
   const drive = google.drive({version: 'v3', auth});
   const driveactivity=google.driveactivity({version: 'v2', auth});
-
   const fileList = [];
   let NextPageToken = "";
   do {
     const params = {
-      orderBy:"name",
       pageToken: NextPageToken || "",
       pageSize: 300,
       fields: "nextPageToken, files(id, name, owners)",
@@ -95,25 +87,29 @@ async function listFiles(auth) {
     res.data.files.map((file)=>{
       var owners=file.owners;
       owners.map(async (owner)=>{
-        //var d=`${file.id}***${owner.emailAddress}***${file.name}***`;
+        var d=`${file.id}***${owner.emailAddress}***${file.name}***`;
         driveactivity.activity.query({
           requestBody:{
             itemName:`items/${file.id}`
           }
         },(err,res2)=>{
           if(err) console.log("API RETURNED AN ERROR: "+err);
-                d=`${file.id}***${file.owners.map((owner)=>owner.emailAddress)}***${file.name}***${res2.data.activities.map((activity)=>activity.timestamp)}-${res2.data.activities.map((activity)=>activity.actors.map((actor)=>actor.user.knownUser.personName))}*&*`;
-                drive_activity_file.write(d+'\n');
-                console.log(d);
-          
-          //console.log(d);
-          //drive_activity_file.write(d+'\n');
+          const activities=res2.data.activities;
+          if(activities.length) {
+            activities.map((activity)=>{
+              const target_S=activity.targets;
+              target_S.map((targets)=>{
+                d=d+`${activity.timestamp}-${targets.driveItem.owner.user.knownUser.personName}*&*`;
+              })
+            })
+          }
+          console.log(d);
+          drive_activity_file.write(d+'\n');
         })
       })
-      
+
     })
     NextPageToken = res.data.nextPageToken;
     await sleep1(65000);//Wait for a minute at least as the API allows only 1000 requests per minute.
-
   } while(NextPageToken);
 }
